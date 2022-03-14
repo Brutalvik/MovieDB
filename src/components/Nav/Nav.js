b9a07c7fb0e325e1ecde890c27a81676014001aa
Brutalvik <66142815+Brutalvik@users.noreply.github.com>
2022-03-14 03:48:26 -0400

    Navbar css corrections

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,13 +1,21 @@
 import React from "react";
 import classes from "./Nav.module.css";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Nav = () => {
   const menu = ["Home", "Movies", "TV", "Genres"];
+  const clicked = useSelector((state) => state.headerReducer.clicked);
   return (
     <ul className={classes.nav_container}>
       {menu.map((menuItem, index) => (
-        <Link key={index} to={`/${menuItem}`}>
+        <Link
+          className={`${classes.nav_links} ${
+            !clicked ? classes.light : classes.dark
+          }`}
+          key={index}
+          to={`/${menuItem}`}
+        >
           <li>{menuItem}</li>
         </Link>
       ))}
