e58459e495783bacbda99b5212c11b03e8779dbc
Brutalvik <66142815+Brutalvik@users.noreply.github.com>
2022-03-25 17:07:46 -0400

    Fixed CSs

--- src/components/Player/Tvplayer.js
+++ src/components/Player/Tvplayer.js
@@ -6,62 +6,54 @@ import classes from "./Player.module.css";
 const Tvplayer = () => {
   const dispatch = useDispatch();
   const seasons = useSelector((state) => state.showReducer.playSelectedShow);
-  const show = useSelector((state) => state.tvReducer.show);
-  // console.log(seasons);
+  const show = useSelector((state) => console.log(state.tvReducer));
 
-  const TV_ENDPOINT = `${process.env.REACT_APP_FRAME}tv?id=`;
   window.alias_open = window.open;
 
-  const handleSeasonChange = (event) => {
+  const handleChange = (event) => {
     event.preventDefault();
-    dispatch(tvActions.setShow({ [event.target.name]: event.target.value }));
+    console.log(event.target.value);
+    dispatch(
+      tvActions.setShow({
+        [event.target.name]: event.target.value,
+      })
+    );
   };
-
-  let episodes = [];
 
   const Season = () => {
     return (
       <div>
-        <div>
-          <select onChange={handleSeasonChange} name="season">
-            {seasons.seasons?.map((season, index) => (
-              <option key={index} value={season.season_number}>
-                {season.season_number}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div></div>
+        <select
+          name="season"
+          onChange={handleChange}
+          value={show ? show.season : 1}
+        >
+          {seasons.seasons?.map((season, index) => (
+            <option key={index}>{season.season_number}</option>
+          ))}
+        </select>
       </div>
     );
   };
 
-  // const Episodes = () => {
-  //   let ep = [];
-  //   return (
-  //     <select>
-  //       {seasons.seasons?.map((season, index) => {
-  //         if (selectedSeason === season.season_number) {
-  //           for (let i = 0; i <= season.episode_count; i++) {
-  //             ep.push(i);
-  //           }
-  //         }
-  //         ep?.map((e, i) => (
-  //           <option key={i} value={e}>
-  //             {e}
-  //           </option>
-  //         ));
-  //       })}
-  //     </select>
-  //   );
-  // };
+  const Episodes = () => {
+    return (
+      <select name="episodes" onChange={handleChange}>
+        {seasons.seasons?.map((season, index) => (
+          <option>{}</option>
+        ))}
+      </select>
+    );
+  };
 
   return (
     <div>
       <div>
         <Season />
       </div>
-      <div></div>
+      <div>
+        <Episodes />
+      </div>
     </div>
   );
 };
--- src/components/Trendingtv/Trendingtv.js
+++ src/components/Trendingtv/Trendingtv.js
@@ -2,7 +2,6 @@ import React from "react";
 import classes from "./Trendingtv.module.css";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { showActions } from "../../store/showReducer";
 import { getPopularTv } from "../../store/tvActions";
 import { getTvShow } from "../../store/tvActions";
 
