d14274a6fc6be245d7b7ea6fef6e959bd78ae6dc
Brutalvik <66142815+Brutalvik@users.noreply.github.com>
2022-03-26 00:42:35 -0400

    Added back button to Player

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -3,8 +3,11 @@ import classes from "./Player.module.css";
 import { useSelector } from "react-redux";
 import Movieplayer from "./Movieplayer";
 import Error from "../404/Errorpage";
+import Button from "../../UI/Button/Button";
+import { useNavigate } from "react-router-dom";
 
 const Player = () => {
+  const navigate = useNavigate();
   const show = useSelector((state) => state.showReducer.playSelectedShow);
 
   const moviePlayer = (
@@ -15,6 +18,9 @@ const Player = () => {
         </div>
       </div>
       {show.media_type === "movie" ? <Movieplayer /> : <Error />}
+      <div className={classes.back}>
+        <Button text="Back" onClick={() => navigate(-1)} />
+      </div>
     </div>
   );
 
