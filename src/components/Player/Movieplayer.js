0810f02530f25451a1a8a386eb3b6d38ecf4c09b
vikramkumar-ibm <vikramkumar@ibm.com>
2022-07-02 15:34:59 -0600

    Changes to moviedb api

--- src/components/Player/Movieplayer.js
+++ src/components/Player/Movieplayer.js
@@ -1,18 +1,19 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import classes from "./Player.module.css";
+import React from 'react';
+import { useSelector } from 'react-redux';
+import classes from './Player.module.css';
 
 const Movieplayer = () => {
   const show = useSelector((state) => state.showReducer.playSelectedShow);
-  const MOVIE_END_POINT = `${process.env.REACT_APP_FRAME}movie?id=${show.id}`;
+  const showData = sessionStorage.setItem('movie', show);
+  const MOVIE_END_POINT = `${process.env.REACT_APP_FRAME}${showData.id}`;
 
   window.alias_open = window.open;
   return (
     <div className={classes.player_container}>
       <iframe
         src={MOVIE_END_POINT}
-        title="movie frame"
-        allow="fullscreen"
+        title='movie frame'
+        allow='fullscreen'
         className={classes.player}
       ></iframe>
     </div>
